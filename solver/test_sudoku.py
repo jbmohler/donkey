98e0ee4c9953c153096639f68e68bea905812383
Joel B. Mohler <joel@kiwistrawberry.us>
2009-06-20 06:55:02 -0400

    correcting test cases

--- solver/test_sudoku.py
+++ solver/test_sudoku.py
@@ -11,27 +11,31 @@ def cputime(t=0):
 
 
 games=[
-#	"near_worst_case.txt",
-#	"qassim_hamza.txt",
-	"sample_01.txt",
-	"flight_1107_easy.txt",
-	"flight_1107_hard.txt",
-	"top_1465_77.txt"]
+#	"examples/near_worst_case.txt",
+	"examples/qassim_hamza.txt",
+	"examples/sample_01.txt",
+	"examples/flight_1107_easy.txt",
+	"examples/flight_1107_hard.txt",
+	"examples/flight_482_easy.txt",
+	"examples/flight_482_hard.txt",
+	"examples/flight_482_easy2.txt",
+	"examples/flight_482_hard2.txt",
+	"examples/top_1465_77.txt"]
 
 import gc
 gc.disable()
 
 for g in games:
 	print "Game:  %s" % (g,)
-#	from sudoku1 import *
-#	s=Sudoku_FromFile(g)
-#	t1=cputime()
-#	for i in range(10):
-#		s.shuffle()
-#		s.solve()
-#	t2=cputime()
-#	print "Sudoku v. 1:  %.3f" % (t2-t1)
-#	gc.collect()
+	from sudoku1 import *
+	s=Sudoku_FromFile(g)
+	t1=cputime()
+	for i in range(10):
+		s.shuffle()
+		s.solve()
+	t2=cputime()
+	print "Sudoku v. 1:  %.3f" % (t2-t1)
+	gc.collect()
 	
 	from sudoku2 import *
 	s=Sudoku_FromFile(g)
